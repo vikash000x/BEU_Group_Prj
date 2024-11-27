19ce053ab901d5166375e42c1fe95cb4141daa2b
vikashsinha2603 <vikashsinha045@gmail.com>
2024-11-27 22:25:13 +0530

    filter feature in job

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -7,40 +7,31 @@ import { Label } from './label'
 import {filterData} from '../lib/utils' 
 
 
-const FilterCard = () => {
-    // const [selectedValue, setSelectedValue] = useState('');
+
+const FilterCard = ({categories, selectedCategory, onCategoryChange}) => {
    
-    // const changeHandler = (value) => {
-    //     setSelectedValue(value);
-    // }
-    // useEffect(()=>{
-    //     dispatch(setSearchedQuery(selectedValue));
-    // },[selectedValue]);
     return (
-        <div className='w-full bg-white p-3 rounded-md'>
-            <h1 className='font-bold text-lg'>Filter Jobs</h1>
-            <hr className='mt-3' />
-            <RadioGroup >
-                {
-                    filterData.map((data, index) => (
-                        <div>
-                            <h1 className='font-bold text-lg'>{data.fitlerType}</h1>
-                            {
-                                data.array.map((item, idx) => {
-                                    const itemId = `id${index}-${idx}`
-                                    return (
-                                        <div className='flex items-center space-x-2 my-2'>
-                                            <RadioGroupItem value={item} id={itemId} />
-                                            <Label htmlFor={itemId}>{item}</Label>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    ))
-                }
-            </RadioGroup>
-        </div>
+        <div className="w-full bg-white p-3 rounded-md">
+        <h1 className="font-bold text-lg">Filter Jobs</h1>
+        <hr className="mt-3" />
+        <RadioGroup>
+          {filterData.map((data, index) => (
+            <div key={index}>
+              <h1 className="font-bold text-lg">{data.fitlerType}</h1>
+              {data.array.map((item, idx) => {
+                const itemId = `id${index}-${idx}`;
+                return (
+                  <div key={itemId} className="flex items-center space-x-2 my-2">
+                    <RadioGroupItem categories={item} id={itemId}  checked={selectedCategory === item} // Ensures the correct item is selected
+  onClick={() => onCategoryChange(selectedCategory === item ? 'all' : item)}  />
+                    <Label htmlFor={itemId}>{item}</Label>
+                  </div>
+                );
+              })}
+            </div>
+          ))}
+        </RadioGroup>
+      </div>
     )
 }
 
